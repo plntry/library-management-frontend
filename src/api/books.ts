9a19ecb46828cd7b1d28db529b95502d901b58cc
plntry <nedilko7b@gmail.com>
2025-03-29 20:40:31 +0200

    fixed book details api

--- src/api/books.ts
+++ src/api/books.ts
@@ -6,7 +6,7 @@ const BOOKS_BASE_URL = "/api/v1/books";
 export const booksApi = {
   getAll: async () => await api.get(BOOKS_BASE_URL),
   getById: async (bookId: string) =>
-    await api.get(`${BOOKS_BASE_URL}/${bookId}`),
+    await api.get(`${BOOKS_BASE_URL}/id/${bookId}`),
   create: async (body: BookCreateUpdateData) =>
     await api.post(BOOKS_BASE_URL, body),
   update: async (body: BookCreateUpdateData) =>
