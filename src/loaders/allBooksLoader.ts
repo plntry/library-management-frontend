bf4863f8750383629599543cfb5f7a0cb719b4a0
plntry <nedilko7b@gmail.com>
2025-03-29 16:48:32 +0200

    added get books api

--- src/loaders/allBooksLoader.ts
+++ src/loaders/allBooksLoader.ts
@@ -1,18 +1,18 @@
-// import { booksApi } from "../api/books";
-// import { Book } from "../models/Book";
-import { dummyBooks } from "../api/dummyBooks";
+import { booksApi } from "../api/books";
+import { Book } from "../models/Book";
+// import { dummyBooks } from "../api/dummyBooks";
 
 export async function allBooksLoader() {
-  // const response = await booksApi.getAll();
+  const response = await booksApi.getAll();
 
-  // if (response.status === 200) {
-  //   return response.data.map((el: Book) => ({
-  //     ...el,
-  //     key: el.id,
-  //   }));
-  // }
+  if (response.status === 200) {
+    return response.data.map((el: Book) => ({
+      ...el,
+      key: el.id,
+    }));
+  }
 
-  // return [];
+  return [];
 
-  return dummyBooks;
+  // return dummyBooks;
 }
