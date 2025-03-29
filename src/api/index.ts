3ad0ab55feeae67d222a8e530fd0221953df91cd
plntry <nedilko7b@gmail.com>
2025-03-29 08:39:38 +0200

    added using cookies in api calls

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,6 +5,7 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const api = axios.create({
   baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
+  withCredentials: true,
 });
 
 export default api;
